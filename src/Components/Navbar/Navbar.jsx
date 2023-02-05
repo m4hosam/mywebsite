d3b91f9f21375b66a9d8ed776392ec2e61bbd722
MOHAMED HOSAM <mohamedhosaam154@gmail.com>
2023-02-05 20:25:13 +0300

    Navbar Using MUI

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,81 +1,141 @@
-import React, { useEffect, useState } from "react";
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 import { BiMenuAltRight } from "react-icons/bi";
 import { AiOutlineClose } from "react-icons/ai";
-// import { Link, useNavigate } from "react-router-dom";
-import "./navbar.css";
+import { COLORS } from "../Colors"
+// import photo from '../Images/logo.jpg'
 
-function Navbar() {
-    // const navigate = useNavigate();
-    const [menuOpen, setMenuOpen] = useState(false);
-    const [size, setSize] = useState({
-        width: 0,
-        height: 0,
-    });
-    useEffect(() => {
-        const handleResize = () => {
-            setSize({
-                width: window.innerWidth,
-                height: window.innerHeight,
-            });
-        };
-        window.addEventListener("resize", handleResize);
+const drawerWidth = 280;
+const navItems = ['About', 'Experience', 'Projects', 'Contact'];
 
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
+function DrawerAppBar(props) {
+    const { window } = props;
+    const [mobileOpen, setMobileOpen] = React.useState(false);
 
-    useEffect(() => {
-        if (size.width > 768 && menuOpen) {
-            setMenuOpen(false);
-        }
-    }, [size.width, menuOpen]);
-
-    const menuToggleHandler = () => {
-        setMenuOpen((p) => !p);
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
     };
 
-    return (
-        <header className="header">
-            <div className="header__content">
-                <a to="/" className="header__content__logo">
-                    Navbar
-                </a>
-                <nav
-                    className={`${"header__content__nav"} 
-          ${menuOpen && size.width < 768 ? `${"isMenu"}` : ""} 
-          }`}
-                >
-                    <ul>
-                        <li>
-                            <a to="/">Home</a>
-                        </li>
-                        <li>
-                            <a to="/profile">Profile</a>
-                        </li>
-                        <li>
-                            <a to="/Works">Browse Works</a>
-                        </li>
-                        <li>
-                            <a to="/help">Help</a>
-                        </li>
+    const drawer = (
+        <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center', color: COLORS.white }}>
+            <Typography variant="h6" sx={{ my: 2 }}>
+                <img style={{ width: 70, height: 70 }} src={process.env.PUBLIC_URL + '/Images/m4h.png'} />
+            </Typography>
 
-                        <a to="/register">
-                            <button className="btn">Register</button>
-                        </a>
-                        <a to="/login">
-                            <button className="btn btn__login">Login</button>
-                        </a>
-                    </ul>
-                </nav>
-                <div className="header__content__toggle">
-                    {!menuOpen ? (
-                        <BiMenuAltRight onClick={menuToggleHandler} />
-                    ) : (
-                        <AiOutlineClose onClick={menuToggleHandler} />
-                    )}
-                </div>
-            </div>
-        </header>
+            <Divider sx={{ mx: 5, my: 5 }} color='#fff' />
+
+            <List>
+                {navItems.map((item) => (
+                    <ListItem key={item} disablePadding>
+                        <ListItemButton sx={{
+                            margin: '1rem',
+                            color: COLORS.white, backgroundColor: COLORS.black3,
+                            ':hover': {
+                                bgcolor: COLORS.black4,
+                            }, textAlign: 'center'
+                        }}>
+                            <ListItemText primary={item} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    );
+
+    const container = window !== undefined ? () => window().document.body : undefined;
+
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <AppBar component="nav" sx={{ backgroundColor: COLORS.black1 }}>
+                <Toolbar>
+
+                    <Typography
+                        component="div"
+                        sx={{ flexGrow: 1, display: { xs: 'block', sm: 'block' } }}
+                    >
+                        <img style={{ width: 70, height: 70 }} src={process.env.PUBLIC_URL + '/Images/logo.jpg'} />
+                    </Typography>
+
+
+
+                    <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+                        {navItems.map((item) => (
+                            <Button key={item} sx={{
+                                marginRight: '1rem',
+                                color: COLORS.white, backgroundColor: COLORS.black1,
+                                ':hover': {
+                                    bgcolor: COLORS.black4,
+                                }
+                            }}>
+                                {item}
+                            </Button>
+                        ))}
+                    </Box>
+
+                    <IconButton
+
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                        onClick={handleDrawerToggle}
+                        sx={{ display: { sm: 'none' }, marginLeft: 'auto', justifyContent: 'flex-end' }}
+                    >
+                        {!mobileOpen ? (
+                            <BiMenuAltRight />
+                        ) : (
+                            <AiOutlineClose />
+                        )}
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+            <Box component="nav" >
+                <Drawer
+                    PaperProps={{
+                        sx: {
+                            backgroundColor: COLORS.black3
+                        }
+                    }}
+                    container={container}
+                    variant="temporary"
+                    open={mobileOpen}
+                    onClose={handleDrawerToggle}
+                    ModalProps={{
+                        keepMounted: true, // Better open performance on mobile.
+                    }}
+                    sx={{
+                        display: { xs: 'block', sm: 'none' },
+                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                    }}
+                >
+                    {drawer}
+                </Drawer>
+            </Box>
+            <Box component="main" sx={{ p: 3 }}>
+                <Toolbar />
+
+            </Box>
+        </Box>
     );
 }
 
-export default Navbar;
+DrawerAppBar.propTypes = {
+    /**
+     * Injected by the documentation to work in an iframe.
+     * You won't need it on your project.
+     */
+    window: PropTypes.func,
+};
+
+export default DrawerAppBar;
