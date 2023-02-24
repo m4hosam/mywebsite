706b2a00986aff8f90e27862a50985488758ce11
MOHAMED HOSAM <mohamedhosaam154@gmail.com>
2023-02-24 15:53:28 +0300

    Updates

---
+++ precache-manifest.118b2ac2193e5454636a26afcfe6621d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae5a494601955fb58d0d",
+    "url": "/static/css/main.c5251590.chunk.css"
+  },
+  {
+    "revision": "ae5a494601955fb58d0d",
+    "url": "/static/js/main.bc2c5518.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1bc0bd8c06bff1b1a5f6",
+    "url": "/static/js/2.81cac9e9.chunk.js"
+  },
+  {
+    "revision": "b713f5659de4a2583f4fc8e058970d82",
+    "url": "/static/media/webScrap.b713f565.png"
+  },
+  {
+    "revision": "30558e32a7fc116c5a0ac457aed3c882",
+    "url": "/static/media/resume.30558e32.pdf"
+  },
+  {
+    "revision": "2fe0622df8f6dddc7773accc21738e39",
+    "url": "/static/media/logo.2fe0622d.png"
+  },
+  {
+    "revision": "d736a6d59a3225e099eb7b67e7035824",
+    "url": "/static/media/m4h.d736a6d5.png"
+  },
+  {
+    "revision": "bda779ee40cd115701b16811ba5125e5",
+    "url": "/static/media/interactiveCommentSection1.bda779ee.png"
+  },
+  {
+    "revision": "eda2402aa84e3f7a1bb0a57856a7ca85",
+    "url": "/static/media/bankMangementSystem.eda2402a.png"
+  },
+  {
+    "revision": "759fb665e70ba27ddf50247a37cd4942",
+    "url": "/static/media/HarryPotter.759fb665.png"
+  },
+  {
+    "revision": "80f7c571efb8ae4c7e6d8e4223b78c22",
+    "url": "/index.html"
+  }
+];
